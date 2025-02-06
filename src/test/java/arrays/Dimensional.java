d3b23e1e3a6d6c7ac2d9d42694761e738258f4fd
Atik147 <atikalkaran@gmail.com>
2025-02-06 00:16:46 -0500

    This is my first commiting java code

---
+++ src/test/java/arrays/Dimensional.java
@@ -0,0 +1,23 @@
+package arrays;
+
+public class Dimensional {
+
+	public static void main(String[] args) {
+		
+		
+		int a[][] = {
+				
+				
+				{100,200},
+				{300,400},
+				{500,600},
+		};
+		
+		System.out.println(a.length);
+		System.out.println(a[1].length);
+
+	}
+		
+	
+
+}
